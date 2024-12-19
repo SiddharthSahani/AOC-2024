54fde43a43b9f77fc9d96fc9c494009517465766
SiddharthSahani <sidhucroedde@gmail.com>
2024-12-19 23:22:51 +0530

    added day-19 code

---
+++ day-19/part-2.py
@@ -0,0 +1,77 @@
+
+from collections import deque
+from functools import cache
+
+
+with open('day-19/input/part-2.txt') as f:
+    cur_patterns, desired_patterns = f.read().split('\n\n')
+    cur_patterns = cur_patterns.split(', ')
+    desired_patterns = desired_patterns.split('\n')
+
+
+class TrieNode:
+    def __init__(self, char):
+        self.char = char
+        self.is_end = False
+        self.children = {}
+
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode("")
+
+    def insert(self, word):
+        node = self.root
+        for char in word:
+            if char in node.children:
+                node = node.children[char]
+            else:
+                new_node = TrieNode(char)
+                node.children[char] = new_node
+                node = new_node
+        node.is_end = True
+
+    def dfs(self, node, pre):
+        if node.is_end:
+            self.output.append((pre + node.char))
+        for child in node.children.values():
+            self.dfs(child, pre + node.char)
+
+    @cache
+    def search(self, x):
+        node = self.root
+        for char in x:
+            if char in node.children:
+                node = node.children[char]
+            else:
+                return []
+
+        self.output = []
+        self.dfs(node, x[:-1])
+        return self.output
+
+
+trie = Trie()
+for pattern in cur_patterns:
+    trie.insert(pattern)
+
+
+@cache
+def is_valid(d_pattern):
+    if len(d_pattern) == 0:
+        return True
+
+    pos = trie.search(d_pattern[0])
+    return sum(
+        is_valid(d_pattern[len(p):])
+        for p in pos
+        if d_pattern.startswith(p)
+    )
+
+
+s = sum(
+    is_valid(pattern)
+    for pattern in desired_patterns
+)
+print(s)
